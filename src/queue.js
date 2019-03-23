925205cc8c326d6f9b3ac5820d877b08f0c4ae30
Vladimir Kovalchuk <Vladimir6332@gmail.com>
2019-03-24 02:48:46 +0300

    feat: add queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,25 +1,41 @@
 const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
-	constructor(maxSize) {
+    constructor(maxSize = 30) {
+        this.maxSize = maxSize;
+        this.heap = new MaxHeap();
+    }
 
-	}
+    push(data, priority) {
+        if (this.size() == this.maxSize) throw new ExceptionQueue("Queue has max size.");
+        else
+            this.heap.push(data, priority);
 
-	push(data, priority) {
+    }
 
-	}
+    shift() {
+        if (this.isEmpty()) throw new ExceptionQueue("Queue is emty.");
+        else
+            return this.heap.pop();
 
-	shift() {
+    }
 
-	}
+    size() {
+        return this.heap.size();
 
-	size() {
 
-	}
+    }
 
-	isEmpty() {
-		
-	}
+    isEmpty() {
+        return this.heap.isEmpty();
+
+    }
+
+    ExceptionQueue(message) {
+        this.message = message;
+        this.name = "Exception queue:";
+    }
+
 }
 
 module.exports = PriorityQueue;
